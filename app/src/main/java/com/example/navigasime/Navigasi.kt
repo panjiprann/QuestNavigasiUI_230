7ec029695ff68e96c9805db74adb51264249f4b3
panjiprann <panjipranantya20@gmail.com>
2025-11-06 09:21:58 +0700

    feat: Implementasi NavHost di DataApp
    Perubahan ini mengimplementasikan `NavHost` di dalam `DataApp` menggunakan `Scaffold`. `NavHost` diatur untuk memulai navigasi dari tujuan `Navigasi.Formulir`.

--- app/src/main/java/com/example/navigasime/Navigasi.kt
+++ app/src/main/java/com/example/navigasime/Navigasi.kt
@@ -1,5 +1,10 @@
 package com.example.navigasime
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulir,
     Detail
@@ -7,8 +12,13 @@ enum class Navigasi {
 
 @composable
 fun DataApp(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir
+    }
 
 }
