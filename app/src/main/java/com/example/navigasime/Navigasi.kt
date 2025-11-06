d389a71693295a0c619a5a89402d2af925293980
panjiprann <panjipranantya20@gmail.com>
2025-11-06 09:14:40 +0700

    menambahkan enum di navigasi

--- app/src/main/java/com/example/navigasime/Navigasi.kt
+++ app/src/main/java/com/example/navigasime/Navigasi.kt
@@ -1,2 +1,14 @@
 package com.example.navigasime
 
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+    modifier: Modifier
+){
+
+}
