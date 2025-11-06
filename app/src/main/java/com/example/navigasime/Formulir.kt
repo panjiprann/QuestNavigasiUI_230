24cb3f5ad8cd75112f7549e6002860dc379a01d4
panjiprann <panjipranantya20@gmail.com>
2025-11-06 19:39:06 +0700

    feat: Implementasi FormIsian Composable
    Perubahan ini membuat composable `FormIsian` yang berisi `Scaffold` dengan `TopAppBar` dan sebuah `OutlinedTextField` untuk input nama.
    
    - Menambahkan `Scaffold` dengan `TopAppBar` yang menampilkan judul dari resource string.
    - Di dalam `Scaffold`, `Column` digunakan untuk menata `OutlinedTextField`.
    - `OutlinedTextField` ditambahkan untuk input "Nama Lengkap".

--- app/src/main/java/com/example/navigasime/Formulir.kt
+++ app/src/main/java/com/example/navigasime/Formulir.kt
@@ -1,2 +1,55 @@
 package com.example.navigasime
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-Laki", "Perempuan"),
+    OnSubmitBtnClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700),
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {}
+            )
+        }
+    }
+}
